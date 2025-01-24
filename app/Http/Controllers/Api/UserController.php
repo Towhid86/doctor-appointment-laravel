6fec7efdfec798222a32ca6c2d08bd55807d0bd3
Towhid86 <towhiduzzaman.22@gmail.com>
2025-01-24 22:16:07 +0600

    user api  modify

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -15,8 +15,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
-        return response()->json($users);
+        $user = User::findOrFail(auth()->id());
+        return response()->json($user);
     }
 
     public function login(Request $request)
